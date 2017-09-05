c27707902cc2f5f06b4f4748d33ddcdd9e4f2614
connor-hyman <31594745+connor-hyman@users.noreply.github.com>
2017-09-05 19:23:24 -0400

    Initial upload, week one homework

---
+++ assignment1_connorhyman/sketch.js
@@ -0,0 +1,139 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+    background(31, 93, 158);
+    
+    noStroke();
+    fill(89, 21, 1);
+    rect(100,10,440,100,120,120,120,120);
+    
+    noStroke();
+    fill(255,218,212);
+    ellipse(320,240,450,450);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(170,60,100,100);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(250,50,80,80);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(110,120,110,110);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(460,60,100,100);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(390,40,80,80);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(250,40,80,80);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(130,50,85,85);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(510,50,85,85);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(450,30,85,85);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(190,40,80,80);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(320,30,100,100);
+    
+    noStroke();
+    fill(89,21,1);
+    ellipse(530,120,110,110);
+
+    fill(0,0,0);    
+    ellipse(340,260,20,10);
+    
+    fill(0,0,0);
+    ellipse(300,260,20,10);
+    
+    fill(255,255,255);
+    ellipse(225,165,190,90);
+    
+    fill(31, 93, 158);
+    ellipse(225,165,60,60);
+    
+    fill(0,0,0);
+    ellipse(225,165,20,20);
+    
+    fill(255,255,255);
+    ellipse(235,160,10,10);
+    
+    fill(255,255,255);
+    ellipse(425,165,137,200);
+    
+    fill(31, 93, 158);
+    ellipse(425,165,110,89);
+    
+    fill(0,0,0);
+    ellipse(425,165,20,80);
+    
+    fill(255,255,255);
+    ellipse(435,145,20,10);
+    
+    fill(255,218,212);
+    ellipse(95,220,80,100);
+    
+    fill(255,218,212);
+    ellipse(545,220,80,100);
+    
+    fill(0,0,0);
+    strokeWeight(2);
+    stroke(255,26,74);
+    rect(220,320,205,85,0,0,120,120);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(235,325,40,40);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(280,325,40,40);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(325,325,40,40);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(370,325,40,40);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(240,369,40,30,0,0,0,120);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(285,369,35,30);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(325,369,35,30);
+    
+    noStroke();
+    fill(255,255,255);
+    rect(365,369,40,30,0,0,120,0);
+    
+    
+    
+}
